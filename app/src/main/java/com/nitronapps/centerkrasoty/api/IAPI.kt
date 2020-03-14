511b18a5a6c3db9654dfbce4c6bfd456874e32ae
Nikita Varlamov <nvar3@yandex.ru>
2020-03-14 15:45:13 +0500

    added order model

--- app/src/main/java/com/nitronapps/centerkrasoty/api/IAPI.kt
+++ app/src/main/java/com/nitronapps/centerkrasoty/api/IAPI.kt
@@ -6,35 +6,67 @@ import retrofit2.http.*
 
 interface IAPI {
 
+    @FormUrlEncoded
+    @POST("addTransaction/")
+    @Headers("User-Agent: Center Krasoty Https Nitron Apps Burzhua Web Connector", "Connection: close")
+    fun addTransaction(@Field("token") token: String,
+                       @Field("orders") orders: String): Observable<TransactionResponse>
+
+    @FormUrlEncoded
+    @POST("deleteOrder/")
+    @Headers("User-Agent: Center Krasoty Https Nitron Apps Burzhua Web Connector", "Connection: close")
+    fun deleteOrder(@Field("token") token: String, 
+                    @Field("orderId") orderId: Int): Observable<DeleteOrderResponse>
+
+    @FormUrlEncoded
+    @POST("getOffices/")
     @Headers("User-Agent: Center Krasoty Https Nitron Apps Burzhua Web Connector")
+    fun getOffices(@Field("token") token: String): Observable<OfficeResponse>
+
+    @FormUrlEncoded
+    @POST("getOrders/")
+    @Headers("User-Agent: Center Krasoty Https Nitron Apps Burzhua Web Connector")
+    fun getOrders(@Field("token") token: String,
+                  @Field("groupServiceId") groupServiceId: Int,
+                  @Field("date") date: String,
+                  @Field("forUser") forUser: Boolean): Observable<OrderResponse>
+
+    @FormUrlEncoded
+    @POST("getPlaces/")
+    @Headers("User-Agent: Center Krasoty Https Nitron Apps Burzhua Web Connector")
+    fun getPlaces(@Field("token") token: String,
+                  @Field("serviceId") serviceId: Int): Observable<PlaceResponse>
+
+    @FormUrlEncoded
+    @POST("getServices/")
+    @Headers("User-Agent: Center Krasoty Https Nitron Apps Burzhua Web Connector")
+    fun getServices(@Field("token") token: String,
+                    @Field("officeId") officeId: Int): Observable<ServiceResponse>
+
+    @FormUrlEncoded
+    @POST("getUser/")
+    @Headers("User-Agent: Center Krasoty Https Nitron Apps Burzhua Web Connector")
+    fun getUser(@Field("token") token: String): Observable<UserResponse>
+
+    @FormUrlEncoded
+    @POST("loginByPass/")
+    @Headers("User-Agent: Center Krasoty Https Nitron Apps Burzhua Web Connector")
+    fun loginByPassword(@Field("login") login: String,
+                        @Field("password") password: String,
+                        @Field("phone") phone: String): Observable<AuthResponse>
+
     @FormUrlEncoded
     @POST("registerNewUser/")
+    @Headers("User-Agent: Center Krasoty Https Nitron Apps Burzhua Web Connector")
     fun registerNewUser(@Field("login") login: String,
                         @Field("password") password: String,
                         @Field("name") name: String,
                         @Field("surname") surname: String,
-                        @Field("phone") phone: String) : Observable<AuthResponse>
+                        @Field("phone") phone: String): Observable<AuthResponse>
 
-    @Headers("User-Agent: Center Krasoty Https Nitron Apps Burzhua Web Connector")
     @FormUrlEncoded
     @POST("smsRequest/")
+    @Headers("User-Agent: Center Krasoty Https Nitron Apps Burzhua Web Connector")
     fun smsRequest(@Field("login") login: String,
-                   @Field("code") code: Int) : Observable<SMSResponse>
-
-    @Headers("User-Agent: Center Krasoty Https Nitron Apps Burzhua Web Connector")
-    @FormUrlEncoded
-    @POST("loginByPass/")
-    fun loginByPassword(@Field("login") login: String,
-                        @Field("password") password: String,
-                        @Field("phone") phone: String) : Observable<AuthResponse>
-
-    @Headers("User-Agent: Center Krasoty Https Nitron Apps Burzhua Web Connector")
-    @FormUrlEncoded
-    @POST("getOffices/")
-    fun getOffices(@Field("token") token: String): Observable<OfficeResponse>
-
-    @Headers("User-Agent: Center Krasoty Https Nitron Apps Burzhua Web Connector")
-    @FormUrlEncoded
-    @POST("getServices/")
-    fun getServices(@Field("token") token: String, @Field("officeId") officeId: Int): Observable<ServiceResponse>
+                   @Field("code") code: Int): Observable<SMSResponse>
 }
---
+++ app/src/main/java/com/nitronapps/centerkrasoty/model/Order.kt
@@ -0,0 +1,61 @@
+package com.nitronapps.centerkrasoty.model
+
+import com.google.gson.annotations.SerializedName
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.jvm.internal.Intrinsics
+
+
+data class Order(val date: String,
+                 val finishTime: String,
+                 val id: Int,
+                 val login: String,
+                 val placeId: Int,
+                 val price: Double,
+                 @SerializedName("name") val serviceName: String,
+                 val startTime: String,
+                 val time: Long) {
+
+    fun getStartTimeParsed(): Date {
+        val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
+
+
+        return formatter.parse(date + " " + startTime)!!
+    }
+
+    fun getFinishTimeParsed(): Date {
+        val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
+
+
+        return formatter.parse(date + " " + finishTime)!!
+    }
+
+    fun getDay(): String {
+        val formatter =
+            SimpleDateFormat("dd MM yyyy", Locale("ru", "RU"))
+
+        val parse =
+            SimpleDateFormat("yyyy-MM-dd", Locale("ru", "RU")).parse(date)
+
+        return formatter.format(parse!!)
+    }
+
+    fun getStartTimeString(): String {
+        val format =
+            SimpleDateFormat("HH:mm", Locale("ru", "RU")).format(getStartTimeParsed())
+
+        return format
+    }
+
+    fun getFinishTimeString(): String {
+        val format =
+            SimpleDateFormat("HH:mm", Locale("ru", "RU")).format(getFinishTimeParsed())
+
+        return format
+    }
+
+    fun getTimeRange(): String {
+
+        return getStartTimeString() + " - " + getFinishTimeParsed()
+    }
+}
